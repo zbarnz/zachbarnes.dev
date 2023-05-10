e0a1b670863ddc2d104da898453fc7c95bb1ab88
zbarnz <zbarnz@yahoo.com>
2023-05-10 00:45:19 -0600

    changed link and meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <title>Zach Barnes</title>
           <meta
             name="description"
-            content="Created by Zach Barnes using Next.js"
+            content="Zach's Portfolio"
           />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
